4aac50f4c4dccfa3852794f22069a8a46ecbdd3b
MOIZSIDDIQE <moizpc2016@gmail.com>
2024-07-23 01:48:32 +0500

    game changes

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+import chalk from "chalk";
 import inquirer from "inquirer";
 class player {
     name;
@@ -43,7 +44,9 @@ let opponent_name_asking = await inquirer.prompt([
 let player1 = new player(player_name_asking.player_name);
 let opponent1 = new opponent(opponent_name_asking.select);
 do {
-    if (opponent_name_asking.select === "Skeleton") {
+    if (opponent_name_asking.select === "Skeleton" ||
+        opponent_name_asking.select === "Alien" ||
+        opponent_name_asking.select === "Zombie") {
         let ask = await inquirer.prompt([
             {
                 name: "function",
@@ -56,107 +59,39 @@ do {
             let num = Math.floor(Math.random() * 2);
             if (num > 0) {
                 player1.fuel_decreses();
-                console.log(`${player1.name} fuel is ${player1.fuel}`);
-                console.log(`${opponent1.name} fuel is ${opponent1.fuel}`);
+                console.log("--------------------------------------------------------");
+                console.log(chalk.bold.red(`${player1.name} fuel is ${player1.fuel}`));
+                console.log(chalk.bold.green(`${opponent1.name} fuel is ${opponent1.fuel}`));
+                console.log("--------------------------------------------------------");
                 if (player1.fuel <= 0) {
-                    console.log(`You Loose, Better Luck Next Time`);
+                    console.log(chalk.bold.red("###########################################"));
+                    console.log(chalk.bold.italic.red(`You Loose 😔😔😔, Better Luck Next Time👍`));
+                    console.log(chalk.bold.red("###########################################"));
                     process.exit();
                 }
             }
             if (num <= 0) {
                 opponent1.fuel_decreses();
-                console.log(`${player1.name} fuel is ${player1.fuel}`);
-                console.log(`${opponent1.name} fuel is ${opponent1.fuel}`);
+                console.log("--------------------------------------------------------");
+                console.log(chalk.bold.green(`${player1.name} fuel is ${player1.fuel}`));
+                console.log(chalk.bold.red(`${opponent1.name} fuel is ${opponent1.fuel}`));
+                console.log("--------------------------------------------------------");
                 if (opponent1.fuel <= 0) {
-                    console.log(`You Win The Game`);
+                    console.log("##################");
+                    console.log(chalk.bold.italic.yellow(`You Win The Game`));
+                    console.log("##################");
                     process.exit();
                 }
             }
         }
         else if (ask.function === "Drink portion") {
             player1.Fuel_Increase();
-            console.log(`you Drink Health Portion your Fuel is ${player1.fuel}`);
+            console.log(chalk.bold.italic.green(`you Drink Health Portion your Fuel is ${player1.fuel}`));
         }
         else if (ask.function === "Run for your Life....") {
-            console.log(`You Loose, Better Luck Next Time`);
-            process.exit();
-        }
-    }
-    if (opponent_name_asking.select === "Alien") {
-        let ask = await inquirer.prompt([
-            {
-                name: "function",
-                type: "list",
-                message: "What would you like to do?",
-                choices: ["Attack", "Drink portion", "Run for your Life...."]
-            }
-        ]);
-        if (ask.function === "Attack") {
-            let num = Math.floor(Math.random() * 2);
-            if (num > 0) {
-                player1.fuel_decreses();
-                console.log(`${player1.name} fuel is ${player1.fuel}`);
-                console.log(`${opponent1.name} fuel is ${opponent1.fuel}`);
-                if (player1.fuel <= 0) {
-                    console.log(`You Loose, Better Luck Next Time`);
-                    process.exit();
-                }
-            }
-            if (num <= 0) {
-                opponent1.fuel_decreses();
-                console.log(`${player1.name} fuel is ${player1.fuel}`);
-                console.log(`${opponent1.name} fuel is ${opponent1.fuel}`);
-                if (opponent1.fuel <= 0) {
-                    console.log(`You Win The Game`);
-                    process.exit();
-                }
-            }
-        }
-        else if (ask.function === "Drink portion") {
-            player1.Fuel_Increase();
-            console.log(`you Drink Health Portion your Fuel is ${player1.fuel}`);
-        }
-        else if (ask.function === "Run for your Life....") {
-            console.log(`You Loose, Better Luck Next Time`);
-            process.exit();
-        }
-    }
-    if (opponent_name_asking.select === "Zombie") {
-        let ask = await inquirer.prompt([
-            {
-                name: "function",
-                type: "list",
-                message: "What would you like to do?",
-                choices: ["Attack", "Drink portion", "Run for your Life...."]
-            }
-        ]);
-        if (ask.function === "Attack") {
-            let num = Math.floor(Math.random() * 2);
-            if (num > 0) {
-                player1.fuel_decreses();
-                console.log(`${player1.name} fuel is ${player1.fuel}`);
-                console.log(`${opponent1.name} fuel is ${opponent1.fuel}`);
-                if (player1.fuel <= 0) {
-                    console.log(`You Loose, Better Luck Next Time`);
-                    process.exit();
-                }
-            }
-            if (num <= 0) {
-                opponent1.fuel_decreses();
-                console.log(`${player1.name} fuel is ${player1.fuel}`);
-                console.log(`${opponent1.name} fuel is ${opponent1.fuel}`);
-                if (opponent1.fuel <= 0) {
-                    console.log(`You Win The Game`);
-                    process.exit();
-                }
-            }
-        }
-        else if (ask.function === "Drink portion") {
-            player1.Fuel_Increase();
-            console.log(`you Drink Health Portion your Fuel is ${player1.fuel}`);
-        }
-        else if (ask.function === "Run for your Life....") {
-            console.log(`You Loose, Better Luck Next Time`);
+            console.log(chalk.bold.red("###########################################"));
+            console.log(chalk.bold.italic.red(`You Loose 😔😔😔, Better Luck Next Time 👍`));
+            console.log(chalk.bold.red("###########################################"));
             process.exit();
         }
     }
